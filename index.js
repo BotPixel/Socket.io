85c9ddb1966adc4613b56d5cf0ccedcd71e89520
BotPixel <46143060+BotPixel@users.noreply.github.com>
2020-01-25 00:23:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat', function(msg){
-    io.emit('chat', msg);
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
